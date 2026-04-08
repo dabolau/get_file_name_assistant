eebecf9bed53899b01352654c375eb8f9e339037
dabolau <dabolau@qq.com>
2026-04-08 10:29:49 +0800

    更新官网地址

--- lib/url.dart
+++ lib/url.dart
@@ -4,7 +4,7 @@ import 'package:url_launcher/url_launcher.dart';
 class UrlHelper {
   // 打开官方网站链接
   static Future<void> openWebsite() async {
-    final Uri url = Uri.parse('https://your-website.com');
+    final Uri url = Uri.parse('https://dabolau.pages.dev/');
     if (!await launchUrl(url)) {
       debugPrint('Could not launch the website');
     }
